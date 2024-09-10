7be0b17b20040558f75a3cfd27dddd0419abd169
JTech <58959713+JTech-Dev@users.noreply.github.com>
2024-09-10 19:39:25 -0400

    New Component
    - Added Tax Calculator

---
+++ JTechPorfolio/Models/TaxCalculatorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JTechPorfolio.Models;
+
+public class TaxCalculatorModel
+{
+    public decimal InputAmount { get; set; }
+    public decimal TaxOneAmount { get; set; }
+    public decimal TaxTwoAmount { get; set; }
+    public decimal TaxThreeAmount { get; set; }
+    public decimal TaxFourAmount { get; set; }
+    public decimal TotalAmount { get; set; }
+}
